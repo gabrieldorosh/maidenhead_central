927bead4458a0d38df848fbc80cf393a4f6049b9
Gabriel Dorosh <gabsdorosh@gmail.com>
2024-12-19 01:12:49 +0000

    Added modals

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -4,13 +4,15 @@ import { AiOutlineMenu } from "react-icons/ai";
 import Avatar from "../Avatar";
 import { useCallback, useState } from "react";
 import MenuItem from "./MenuItem";
+import useRegisterModal from "@/app/hooks/useRegisterModal";
 
 const UserMenu = () => {
+    const registerModal = useRegisterModal();
     const [isOpen, setIsOpen] = useState(false);
 
     const toggleOpen = useCallback(() => {
         setIsOpen((value) => !value);
-    });
+    }, []);
 
     return (
         <div className="relative">
@@ -87,7 +89,7 @@ const UserMenu = () => {
                             label="Log in"
                         />
                         <MenuItem 
-                            onClick={() => {}}
+                            onClick={() => registerModal.onOpen()}
                             label="Sign up"
                         />
                     </>
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css";
 
 import Navbar from "./components/navbar/Navbar";
 import ClientOnly from "./components/ClientOnly";
+import RegisterModal from "./components/modals/RegisterModal";
+import ToasterProvider from "./providers/ToasterProvider";
 
 export const metadata: Metadata = {
   title: "Maidenhead Central",
@@ -23,6 +25,8 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
+          <ToasterProvider />
+          <RegisterModal />
           <Navbar />
         </ClientOnly>
         {children}
