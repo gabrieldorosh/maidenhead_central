ff900c290ba881228dc5c6b2e4a7c1b89bd99b53
Gabriel Dorosh <gabsdorosh@gmail.com>
2024-12-21 19:11:09 +0000

    Added listing retrieval and UI components for property listings

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,22 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: [
-      "avatars.githubusercontent.com",
-      "lh3.googleusercontent.com",
-      "res.cloudinary.com",
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/**",
+      },
     ],
   }
 };
